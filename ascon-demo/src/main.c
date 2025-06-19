04117bb18a83891cbbda24b7fdf6e5d70155a0c5
Dmytro Ochkas <dmytro.ochkas@imt-atlantique.fr>
2025-06-19 17:41:24 +0200

    issue(#8): Studying Ascon AEAD
    - Integrate Ascon lib in the project

--- ascon-demo/src/main.c
+++ ascon-demo/src/main.c
@@ -1,6 +1,14 @@
 
+#include <stdio.h>
 #include <stdlib.h>
 
+#include "ascon.h"
+
 int main() {
+    printf("Hello world");
+
+    // ascon_aead_encrypt();
+    // ascon_aead_decrypt();
+
     return EXIT_SUCCESS;
 }

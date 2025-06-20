d0ed4e3f3b3fed89acfdd4fe5337b75d38f5264f
Dmytro Ochkas <dmytro.ochkas@imt-atlantique.fr>
2025-06-20 12:02:33 +0200

    issue(#8): Studying Ascon AEAD
    - Define tasks for Alejandra

--- ascon-demo/src/main.c
+++ ascon-demo/src/main.c
@@ -4,10 +4,18 @@
 
 #include "ascon.h"
 
-int main() {
-    printf("Hello world");
+// TODO: define Ascon input variables here (e.g secret key)
 
+int main() {
+
+    // TODO: Read the input from the console
+
+    // TODO: Use encrypt function to encrypt the user input
     // ascon_aead_encrypt();
+
+    // TODO: Print the result
+
+    // TODO: Decrypt the encrypted ciphertext and print it
     // ascon_aead_decrypt();
 
     return EXIT_SUCCESS;
